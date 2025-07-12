98bcf589ba35207a868f3107cabadbd1572dfa04
clare <clare_baek@naver.com>
2025-07-12 16:08:42 +0900

    타이틀씬추가

---
+++ bubblewitch/Assets/Script/UI/TitleUI.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.UI.Extensions;
+
+public class TitleUI : MonoBehaviour
+{
+    [SerializeField]
+    private Button _startBtn;
+
+    private void Awake()
+    {
+        _startBtn.SetBtnListnerRemoveAllAndAdd(_OnClickStartBtn);
+    }
+
+    private void _OnClickStartBtn()
+    {
+        SceneManager.LoadScene("Title");
+    }
+}
